dd701682ed75381141aea2203d0b15e92ecd9e42
fatimarafiqui <fatima.rafiqui@gmail.com>
2016-11-20 17:25:31 +0530

    just testing

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge']);
 	}
-	res.send('Error, wrong token');
+	res.send('Error, wrong token ' + req.query['hub.verify_token']);
 });
 
 // to post data
